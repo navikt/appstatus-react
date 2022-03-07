e7c560c4ed486dfdf0773c4ea31a8565fd16bcb0
Aleksei Poroshin <aleksei.poroshin@nav.no>
2022-03-07 15:14:01 +0100

    fiks api version

--- src/appstatus-react/utils/sanityClient.ts
+++ src/appstatus-react/utils/sanityClient.ts
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 import { SanityConfig } from '../types';
 
-export const getAppSanityClient = ({ projectId, dataset, apiVersion = '', token = '' }: SanityConfig) => {
+export const getAppSanityClient = ({ projectId, dataset, apiVersion, token = '' }: SanityConfig) => {
     return sanityClient({
         projectId,
         dataset,
